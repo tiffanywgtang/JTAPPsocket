20515ea3bf9a1844defc5fdbb8bbcfebd3b8e052
tiffanywgtang <tiffanywgtang@hotmail.com>
2018-02-25 23:51:22 -0800

    updated stickers

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const server = require("http").Server();
 var io =require("socket.io")(server);
 
 var allUsers = [];
-
+var allstickers =[];
 io.on("connection", function(socket){
     console.log("connet");
     allUsers.push(socket.id);
     console.log(allUsers);
+   
     
     socket.emit("yourid", socket.id);
     
@@ -16,6 +17,29 @@ io.on("connection", function(socket){
     
     socket.on("mymove", function(data){
         socket.broadcast.emit("newmove", data)
+    })
+    
+    socket.on("stick", function(data){
+        allstickers.push(data);
+        io.emit("newSticker", allstickers);
+    })
+    
+    socket.on("joinRoom", function(data){
+        socket.join(data);
+        socket.myRoom = data;
+        socket.emit("yourid", socket.id);
+         
+            if(!allUsers[data]){
+                allUsers[data]=[];
+                allstickers[data]=[];
+            }
+
+            if(!allstickers[data]){
+                allstickers[data]=[];
+            }
+        allUsers[data].push(socket.id);
+        io.to(data).emit("userJoined", allUsers[data]);
+        io.to(data).emit("newSticker", allstickers[data]);
     })
     
     socket.on("disconnect", function(){
