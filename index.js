8a1322d44d7213ba2d139106cc59539a966b767f
Tiffany Tang <tiffanywgtang@hotmail.com>
2018-03-19 17:38:04 -0700

    update quiz

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const server = require('http').Server();
-const port = process.env.PORT || 10001;
+const port = process.env.PORT || 10003;
 
 var io = require('socket.io')(server);
 
+var Qobj=[];
 var names = [];
-var allmsgs= [];
+var playerScore=0;
 
 io.on("connection", function(socket){
    console.log("user connected"); 
-    console.log(socket.id);
+    
+    
+   socket.on("qSubmit", function(data){
+       Qobj.push(data);
+       console.log(Qobj);
+    })
+    
+    io.emit("quiz",Qobj);
     
     socket.on("uName", function(data){
         console.log("username sent = " +data);
@@ -17,20 +25,85 @@ io.on("connection", function(socket){
         
     });
     
-    socket.on("msg", function(data){
-        console.log("msg sent = " +data);
-        allmsgs.push(data);
-        io.emit("allmsgs", allmsgs);
+    
+    socket.on("playerScore", function(data){
+        socket.player={
+            id:socket.id,
+            name:data.username,
+            playerScore:data.score
+        }
+       
+        console.log("opp score: " +socket.player);
     });
+    
+   io.emit("oppScore", socket.player);
+    
+    
+    socket.on("answerA", function(data){
+        console.log("Host's answers: "+Qobj[0].A);
+        console.log("Player's answers: "+data);
+        
+        var msg=false;
+       if(data == Qobj[0].A){
+           msg=true;
+       } 
+        socket.emit("resultA", msg);
+    });
+    
+    socket.on("answerB", function(data){
+        console.log("Host's answers: "+Qobj[1].A);
+        console.log("Player's answers: "+data);
+        
+        var msg=false;
+       if(data == Qobj[1].A){
+           msg=true;
+       } 
+        socket.emit("resultB", msg);
+    });
+    
+    socket.on("answerC", function(data){
+        console.log("Host's answers: "+Qobj[2].A);
+        console.log("Player's answers: "+data);
+        
+        var msg=0;
+       if(data == Qobj[2].A){
+           msg=10;
+       } 
+        socket.emit("resultC", msg);
+    }); 
+    
+    socket.on("answerD", function(data){
+        console.log("Host's answers: "+Qobj[3].A);
+        console.log("Player's answers: "+data);
+        
+        var msg=0;
+       if(data == Qobj[3].A){
+           msg=10;
+       } 
+        socket.emit("resultD", msg);
+    });
+    
+    socket.on("answerE", function(data){
+        console.log("Host's answers: "+Qobj[4].A);
+        console.log("Player's answers: "+data);
+        
+        var msg=0;
+       if(data == Qobj[4].A){
+           msg=10;
+       } 
+        socket.emit("resultE", msg);
+    });
+    
+    
+    
     
     socket.on("disconnect", function(){
         var index = names.indexOf(socket.id);
         names.splice(index, 1);
         io.emit("names", names);
         console.log("user has disconnected");
-        console.log(socket.id);
+    
     })
-    io.emit("allmsgs", allmsgs);
 });
 
 server.listen(port,(err)=>{
@@ -42,9 +115,4 @@ server.listen(port,(err)=>{
     
     console.log("socket port is running");
 })
-
-
-
-
-
 
